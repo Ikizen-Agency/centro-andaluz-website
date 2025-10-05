6ac7418a9b5ff46c3521f1f0c72da848f650fdc3
Johny A. Pedraza Romero <jhonnyantonio892@gmail.com>
2025-10-05 06:54:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7284202710-f8b6e",
+  "appId": "1:15563815735:web:97ef48af6941bf9206b8b2",
+  "apiKey": "AIzaSyCm6fmrZDqktEvn8OsRpxdKWUJ9829C0U8",
+  "authDomain": "studio-7284202710-f8b6e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "15563815735"
+};
