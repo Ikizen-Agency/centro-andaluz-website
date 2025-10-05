cf486c9b0ece69cece87d1b68b823b6af1af3c92
Johny A. Pedraza Romero <jhonnyantonio892@gmail.com>
2025-10-05 06:09:25 +0000

    En el hero section, cambia a una imagen mas impactante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -24,7 +25,7 @@ export default async function HomePage() {
             src={heroImage.imageUrl}
             alt={heroImage.description}
             fill
-            className="object-cover"
+            className="object-cover object-top"
             priority
             data-ai-hint={heroImage.imageHint}
           />
